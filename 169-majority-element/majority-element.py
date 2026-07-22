efc93ed3d02246bcec44905c74ae6cc178632ce6
Muditsehgal12 <muditsehgal341@gmail.com>
2026-07-22 23:34:22 +0530

    Time: 2 ms (88.94%) | Memory: 13.6 MB (38.97%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,11 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        '''
         f={}
         for i in range(len(nums)):
             f[nums[i]]=f.get(nums[i],0)+1
         for i in f:
             if f[i]>len(nums)/2:
                 return i
-    
+        '''
+        nums.sort()
+        return nums[len(nums)//2]
         
