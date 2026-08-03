97a1febb0dce622ad63b6df02b54282f73045f55
Muditsehgal12 <muditsehgal341@gmail.com>
2026-08-03 20:01:41 +0530

    Time: 2 ms (34.95%) | Memory: 12.3 MB (91.08%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.py
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -9,23 +9,19 @@ class Solution(object):
         stack2=[]
         m=''
         r=''
-        for i in range(len(s)):
-            if s[i]=='#':
-                if stack1:
-                    stack1.pop()
-            else:
-                stack1.append(s[i])
-        for i in stack1:
-            r+=i
+        def check(arr):
+            stack1=[]
+            r=''
+            for i in range(len(arr)):
+                if arr[i]=='#':
+                    if stack1:
+                        stack1.pop()
+                else:
+                    stack1.append(arr[i])
+            for i in stack1:
+                r+=i
+            return r
         
-        for i in range(len(t)):
-            if t[i]=='#':
-                if stack2:
-                    stack2.pop()
-            else:
-                stack2.append(t[i])
-        for i in stack2:
-            m+=i
-        return r==m
+        return check(s)==check(t)
         
                       
