8be1eff45c7bc95c8af878868355f03ecd25634e
Muditsehgal12 <muditsehgal341@gmail.com>
2026-08-04 11:53:37 +0530

    Time: 12 ms (47.99%) | Memory: 12.5 MB (22.34%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.py
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.py
@@ -6,14 +6,14 @@ class Solution(object):
         """
         i=0
         stack=[]
-        while i<len(s):
+        for i in range(len(s)):
             if stack and s[i]=="B" and stack[-1]=="A":
                 stack.pop()
-                i+=1
+                
             elif stack and s[i]=="D" and stack[-1]=="C":
                 stack.pop()
-                i+=1
+                
             else:
                 stack.append(s[i])
-                i+=1
+                
         return len(stack)
