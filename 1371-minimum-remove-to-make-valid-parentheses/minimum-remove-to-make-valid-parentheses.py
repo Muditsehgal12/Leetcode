29c77dd1bccfee25042e81c6ae3cd3dc967f4e8d
Muditsehgal12 <muditsehgal341@gmail.com>
2026-08-15 02:07:58 +0530

    Time: 655 ms (16.37%) | Memory: 16.8 MB (12.87%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -5,18 +5,18 @@ class Solution(object):
         :rtype: str
         """
         stack=[]
-        invalid=[]
+        invalid=set()
         ans=''
         for i in range(len(s)):
             if s[i]=='(':
                 stack.append(i)
             elif not stack and s[i]==')':
-                invalid.append(i)
+                invalid.add(i)
             elif stack and s[i]==')':
                 stack.pop()
         if stack:
             for i in stack:
-                invalid.append(i)
+                invalid.add(i)
         for i in range(len(s)):
             if i not in invalid:
                 ans+=s[i]
